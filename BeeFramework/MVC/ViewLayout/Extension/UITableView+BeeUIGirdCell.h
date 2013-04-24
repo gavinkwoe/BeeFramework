1f8f31ec7b316ad04554bc512f2aa0b032a23cbe
hesonghang <ilikeido@hotmail.com>
2013-04-24 14:38:18 +0800

    add BeeUIGirdCell method with UITableView

---
+++ BeeFramework/MVC/ViewLayout/Extension/UITableView+BeeUIGirdCell.h
@@ -0,0 +1,13 @@
+//
+//  UITableView+BeeUIGirdCell.h
+//  618
+//
+//  Created by he songhang on 13-4-24.
+//  Copyright (c) 2013年 he songhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UITableView_BeeUIGirdCell : NSObject
+
+@end
---
+++ BeeFramework/MVC/ViewLayout/Extension/UITableView+BeeUIGirdCell.m
@@ -0,0 +1,13 @@
+//
+//  UITableView+BeeUIGirdCell.m
+//  618
+//
+//  Created by he songhang on 13-4-24.
+//  Copyright (c) 2013年 he songhang. All rights reserved.
+//
+
+#import "UITableView+BeeUIGirdCell.h"
+
+@implementation UITableView_BeeUIGirdCell
+
+@end
